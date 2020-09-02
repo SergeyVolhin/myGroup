d3e678469b4c238a700adf669524016a62cf49b5
serg.volhin2014@yandex.ru <serg.volhin2014@yandex.ru>
2020-09-02 12:07:36 +0500

    Добавьте файлы проекта.

---
+++ Моя группа/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Моя_группа
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.familiya = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.otchestvo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFam = new System.Windows.Forms.TextBox();
+            this.txtOtch = new System.Windows.Forms.TextBox();
+            this.txtAdr = new System.Windows.Forms.TextBox();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table1
+            // 
+            this.table1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.familiya,
+            this.otchestvo,
+            this.adres,
+            this.number});
+            this.table1.Location = new System.Drawing.Point(16, 217);
+            this.table1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.table1.Name = "table1";
+            this.table1.RowHeadersWidth = 51;
+            this.table1.Size = new System.Drawing.Size(736, 185);
+            this.table1.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Имя";
+            this.name.MinimumWidth = 6;
+            this.name.Name = "name";
+            this.name.Width = 125;
+            // 
+            // familiya
+            // 
+            this.familiya.HeaderText = "Фамилия";
+            this.familiya.MinimumWidth = 6;
+            this.familiya.Name = "familiya";
+            this.familiya.Width = 125;
+            // 
+            // otchestvo
+            // 
+            this.otchestvo.HeaderText = "Отчество";
+            this.otchestvo.MinimumWidth = 6;
+            this.otchestvo.Name = "otchestvo";
+            this.otchestvo.Width = 125;
+            // 
+            // adres
+            // 
+            this.adres.HeaderText = "Адрес";
+            this.adres.MinimumWidth = 6;
+            this.adres.Name = "adres";
+            this.adres.Width = 125;
+            // 
+            // number
+            // 
+            this.number.HeaderText = "Номер";
+            this.number.MinimumWidth = 6;
+            this.number.Name = "number";
+            this.number.Width = 125;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Red;
+            this.btnRemove.Location = new System.Drawing.Point(445, 160);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(149, 49);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Удалить";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAdd.Location = new System.Drawing.Point(603, 160);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(149, 49);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(16, 16);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(265, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtFam
+            // 
+            this.txtFam.Location = new System.Drawing.Point(16, 48);
+            this.txtFam.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFam.Name = "txtFam";
+            this.txtFam.Size = new System.Drawing.Size(265, 22);
+            this.txtFam.TabIndex = 4;
+            // 
+            // txtOtch
+            // 
+            this.txtOtch.Location = new System.Drawing.Point(16, 80);
+            this.txtOtch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtOtch.Name = "txtOtch";
+            this.txtOtch.Size = new System.Drawing.Size(265, 22);
+            this.txtOtch.TabIndex = 5;
+            // 
+            // txtAdr
+            // 
+            this.txtAdr.Location = new System.Drawing.Point(16, 112);
+            this.txtAdr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAdr.Name = "txtAdr";
+            this.txtAdr.Size = new System.Drawing.Size(265, 22);
+            this.txtAdr.TabIndex = 6;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(16, 144);
+            this.txtNum.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(265, 22);
+            this.txtNum.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(292, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(292, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(292, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Отчество";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(292, 112);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Адрес";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(292, 144);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Номер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(768, 416);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtAdr);
+            this.Controls.Add(this.txtOtch);
+            this.Controls.Add(this.txtFam);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.table1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Моя группа";
+            ((System.ComponentModel.ISupportInitialize)(this.table1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn familiya;
+        private System.Windows.Forms.DataGridViewTextBoxColumn otchestvo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFam;
+        private System.Windows.Forms.TextBox txtOtch;
+        private System.Windows.Forms.TextBox txtAdr;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Моя группа/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Моя_группа
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            table1.Rows.Add(txtName.Text, txtFam.Text, txtOtch.Text, txtAdr.Text, txtNum.Text);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            table1.Rows.RemoveAt(table1.SelectedCells[0].RowIndex);
+        }
+    }
+}
